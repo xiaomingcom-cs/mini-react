86b4c18c2ad4ce36d08d563ba91bae6e7b867301
miaomiao <75123667+xiaomingcom-cs@users.noreply.github.com>
2023-08-29 02:53:00 +0000

    根据fiber创建dom

--- src/mini-react/fiber.js
+++ src/mini-react/fiber.js
@@ -1,3 +1,4 @@
+import {renderDom} from './react-dom'
 let nextUnitOfWork = null;
 let rootFiber = null;
 
@@ -11,4 +12,21 @@ export function createRoot(element, container) {
     },
   };
   nextUnitOfWork = rootFiber;
+}
+
+// 执行当前工作单元并设置下一个要执行的工作单元
+function performUnitOfWork(workInProgress) {
+  if (!workInProgress.stateNode) {
+    // 若当前 fiber 没有 stateNode，则根据 fiber 挂载的 element 的属性创建
+    workInProgress.stateNode = renderDom(workInProgress.element);
+  }
+  if (workInProgress.return && workInProgress.stateNode) {
+    // 如果 fiber 有父 fiber且有 dom
+    // 向上寻找能挂载 dom 的节点进行 dom 挂载
+    let parentFiber = workInProgress.return;
+    while (!parentFiber.stateNode) {
+      parentFiber = parentFiber.return;
+    }
+    parentFiber.stateNode.appendChild(workInProgress.stateNode);
+  }
 }
